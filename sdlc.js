4d5c005d4f8819c3ca1d44aa66ca25ae7348dac7
Timothy Childs <timmy03126@gmail.com>
2023-09-15 11:47:15 -0400

    sdlc done

--- sdlc.js
+++ sdlc.js
@@ -19,3 +19,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+let analysisOfRequirements = "This is the second step, where you decide what the requirements  and features the program you are trying to make will need."
+let design = "This is the third step, where you decide how the program will look, what language you'll be writing it in, where you will be writing the code and so on."
+let implementation = "This is where you will code your program"
+let testingAndIntegration = "This is where you will test the code that you made for the program. Also, integrating it by posting your program for your customers to use."
+let maintain = "This is where you update and keep up your program by either adding new features, bug fixing and things of that nature."
