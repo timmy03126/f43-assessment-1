d395aa14d9e9c7b76c4fdf2083c57d818dc839bd
Timothy Childs <timmy03126@gmail.com>
2023-09-15 12:50:07 -0400

    gitInfo done

--- gitInfo.js
+++ gitInfo.js
@@ -10,7 +10,7 @@
 */
 
 //CODE HERE
-
+let gitDefinition = `Git is a program that allows people and teams to track their code. It also allows you to view the history of your code as well as the changes made to it.`
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,7 +18,7 @@
 */
 
 //CODE HERE
-
+let gitHubDefinition = `Github is a website that lets you manage your git repositories. Creates a space for you to manage and view all of your git repositories as well.`
 //////////////////PROBLEM 3////////////////////
 /*
     Create a variable called 'gitInitDefinition'.  
@@ -26,7 +26,7 @@
 */
 
 //CODE HERE
-
+let gitInitDefinition = `git init initializes a local repository in the folder you are currently in.`
 //////////////////PROBLEM 4////////////////////
 /*
     Create a variable called 'gitCloneDefinition'.  
@@ -34,7 +34,7 @@
 */
 
 //CODE HERE
-
+let gitCloneDefinition = `git clone makes a copy of the repository you want and saves it at another location.`
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,7 +42,7 @@
 */
 
 //CODE HERE
-
+let gitStatusDefinition = `git status gives you info on the repo you are in.`
 //////////////////PROBLEM 6////////////////////
 /*
     Create a variable called 'gitAddDefinition'.  
@@ -53,7 +53,9 @@
 */
 
 //CODE HERE
+let gitAddDefinition = `git add adds files that are going to be tracked always.`
 
+let gitAddCode = `git add .`
 //////////////////PROBLEM 7////////////////////
 /*
     Create a variable called 'gitCommitDefinition'.  
@@ -64,8 +66,9 @@
 */
 
 //CODE HERE
+let gitCommitDefinition = `git commit creates a snapshot of the files you are trying to track.`
 
-
+let gitCommitCode = `git commit -m "initial commit" `
 //////////////////PROBLEM 8////////////////////
 /*
     Create a variable called 'gitPushDefinition'.  
@@ -73,3 +76,4 @@
 */
 
 //CODE HERE
+let gitPushDefinition = `git push uploads your local repository content to a remote repository like github.`
